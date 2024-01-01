796a233c8ef72a80deaf7402faf3f702795c333b
spicymustard <155345241+spicymustard@users.noreply.github.com>
2024-01-01 18:38:33 +0900

    create new project

---
+++ OddEvenGame/OddEvenGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OddEvenGame
+//
+//  Created by sky140 on 2024/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
