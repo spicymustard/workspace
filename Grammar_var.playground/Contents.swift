5f32eecfb7b1065ad6d5a4626e29e9d3abdd0575
spicymustard <155345241+spicymustard@users.noreply.github.com>
2024-01-01 18:57:06 +0900

    변수, 상수, 함수

---
+++ Grammar_var.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name: String = "Atlas"
+var age: Int = 20
+
+name
+name = "아틀라스"
+
+age
+age = 30
+
+
+let one: Int = 1
+let two: Int = 2
+
+func sayHello(name: String) -> String {
+    return "Nice to meet you, \(name)"
+}
+
+sayHello(name: "아틀라스")
