17482c5da559014757d607a198f793a380ca8a07
spicymustard <155345241+spicymustard@users.noreply.github.com>
2024-01-02 21:13:32 +0900

    Autolayout 실습

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by sky140 on 2024/01/01.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
